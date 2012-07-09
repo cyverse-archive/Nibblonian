d090092845040db535ba6e87675a2a6b83cdb506
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-07-09 08:44:24 -0700

    Switched over to lein-iplant-rpm.

--- project.clj
+++ project.clj
@@ -21,6 +21,10 @@
              {:resource-paths ["local-conf"],
               :dependencies
               [[clj-http "0.3.2"] [org.cloudhoist/stevedore "0.7.1"]]}}
+  :iplant-rpm {:summary "nibblonian"
+               :dependencies ["iplant-service-config >= 0.1.0-5"]
+               :config-files ["log4j.properties"]
+               :config-path "conf"}
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/",
                  "renci.repository"
@@ -30,5 +34,6 @@
   :aot [nibblonian.core]
   :main nibblonian.core
   :min-lein-version "2.0.0"
-  :plugins [[lein-ring "0.5.4"]]
+  :plugins [[lein-ring "0.5.4"]
+            [org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]]
   :description "RESTful interface into iRODS.")
