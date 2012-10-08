e2838ddef8c05194f3607a075e2c2ea2748dbec8
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-10-08 15:44:09 -0700

    Switched validators over to the new jargon context format.

--- src/nibblonian/validators.clj
+++ src/nibblonian/validators.clj
@@ -4,95 +4,95 @@
         [slingshot.slingshot :only [try+ throw+]]))
 
 (defn user-exists
-  [user]
-  (when-not (user-exists? user)
+  [cm user]
+  (when-not (user-exists? cm user)
     (throw+ {:error_code ERR_NOT_A_USER
              :user user})))
 
 (defn all-users-exist
-  [users]
-  (when-not (every? user-exists? users)
+  [cm users]
+  (when-not (every? #(user-exists? cm %) users)
     (throw+ {:error_code ERR_NOT_A_USER
              :users (filterv
                      #(not (user-exists? %1))
                      users)})))
 
 (defn path-exists
-  [path]
-  (when-not (exists? path)
+  [cm path]
+  (when-not (exists? cm path)
     (throw+ {:error_code ERR_DOES_NOT_EXIST
              :path path})))
 
 (defn all-paths-exist
-  [paths]
-  (when-not (every? exists? paths)
+  [cm paths]
+  (when-not (every? #(exists? cm %) paths)
     (throw+ {:error_code ERR_DOES_NOT_EXIST
-             :paths (filterv #(not (exists? %1)) paths)})))
+             :paths (filterv #(not (exists? cm  %1)) paths)})))
 
 (defn no-paths-exist
-  [paths]
-  (when (some exists? paths)
+  [cm paths]
+  (when (some #(exists? cm %) paths)
     (throw+ {:error_code ERR_EXISTS
-             :paths (filterv #(exists? %) paths)})))
+             :paths (filterv #(exists? cm %) paths)})))
 
 (defn path-readable
-  [user path]
-  (when-not (is-readable? user path)
+  [cm user path]
+  (when-not (is-readable? cm user path)
     (throw+ {:error_code ERR_NOT_READABLE
              :path path
              :user user})))
 
 (defn all-paths-readable
-  [user paths]
-  (when-not (every? #(is-readable? user %) paths)
+  [cm user paths]
+  (when-not (every? #(is-readable? cm user %) paths)
     (throw+ {:error_code ERR_NOT_READABLE
-             :path (filterv #(not (is-readable? user %)) paths)})))
+             :path (filterv #(not (is-readable? cm user %)) paths)})))
 
 (defn path-writeable
-  [user path]
-  (when-not (is-writeable? user path)
+  [cm user path]
+  (when-not (is-writeable? cm user path)
     (throw+ {:error_code ERR_NOT_WRITEABLE
              :path path})))
 
 (defn all-paths-writeable
-  [user paths]
-  (when-not (paths-writeable? user paths)
-    (throw+ {:paths (filterv #(not (is-writeable? user %)) paths)
+  [cm user paths]
+  (when-not (paths-writeable? cm user paths)
+    (throw+ {:paths (filterv #(not (is-writeable? cm user %)) paths)
              :error_code ERR_NOT_WRITEABLE})))
 
 (defn path-not-exists
-  [path]
-  (when (exists? path)
+  [cm path]
+  (when (exists? cm path)
     (throw+ {:path path
              :error_code ERR_EXISTS})))
 
 (defn path-is-dir
-  [path]
-  (when-not (is-dir? path)
+  [cm path]
+  (when-not (is-dir? cm path)
     (throw+ {:error_code ERR_NOT_A_FOLDER
              :path path})))
 
 (defn path-is-file
-  [path]
-  (when-not (is-file? path)
+  [cm path]
+  (when-not (is-file? cm path)
     (throw+ {:error_code ERR_NOT_A_FILE
              :path path})))
 
 (defn path-satisfies-predicate
-  [path pred-func? pred-err]
-  (when-not (pred-func? path)
+  [cm path pred-func? pred-err]
+  (when-not (pred-func? cm  path)
     (throw+ {:paths path
              :error_code pred-err})))
 
 (defn paths-satisfy-predicate
-  [paths pred-func? pred-err]
-  (when-not  (every? true? (mapv pred-func? paths))
+  [cm paths pred-func? pred-err]
+  (when-not  (every? true? (mapv #(pred-func? cm %) paths))
     (throw+ {:error_code pred-err
-             :paths (filterv #(not (pred-func? %)) paths)})))
+             :paths (filterv #(not (pred-func? cm %)) paths)})))
 
 (defn user-owns-paths
-  [user paths]
-  (when-not (every? (partial owns? user) paths)
+  [cm user paths]
+  (when-not (every? (partial #(owns? cm %) user) paths)
     (throw+ {:error_code ERR_NOT_OWNER
              :user user
-             :paths (filterv #(not (partial owns? user)) paths)})))
+             :paths (filterv #(not (partial #(owns? cm %) user)) paths)})))
