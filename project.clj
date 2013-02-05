c9d820e4bb96b19763b9e5e6d87c41871117b9aa
Dennis Roberts <dennis@iplantcollaborative.org>
2013-02-05 15:49:40 -0700

    CORE-3963: switch to the latest version of clj-jargon

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [org.clojure/java.classpath "0.1.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.iplantc/clojure-commons "1.3.1-SNAPSHOT"]
-                 [org.iplantc/clj-jargon "0.2.2-SNAPSHOT"]
+                 [org.iplantc/clj-jargon "0.2.3-SNAPSHOT"]
                  [clj-http "0.3.2"]
                  [com.cemerick/url "0.0.7"]
                  [compojure "1.0.1"]
@@ -21,7 +21,7 @@
          :handler nibblonian.core/app}
   :profiles {:dev
              {:resource-paths ["local-conf"],
-              :dependencies [[clj-http "0.3.2"] 
+              :dependencies [[clj-http "0.3.2"]
                              [org.cloudhoist/stevedore "0.7.1"]
                              [criterium "0.2.1"]]}}
   :iplant-rpm {:summary "nibblonian"
